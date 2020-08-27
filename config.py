ddfc60539be17a125c80abf5f55c19b9551c8947
Matt W <foggyio@outlook.com>
2020-08-27 22:33:29 +0100

    Add delay property

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ X_AUTH_KEY      = "YOUR AUTH KEY"
 X_AUTH_EMAIL    = "YOUR LOGIN EMAIL"
 wanted_type     = ('A', 'CNAME', 'EXAMPLE')
 wanted_name     = ('example.com', 'eg.example.com')
+delay           = x # Wait x seconds between runs  
